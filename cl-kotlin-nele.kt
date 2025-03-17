661474a82971461d99557a463f1d9bd9c5c69142
Aaron S Varghese <aaronsvarghese006@gmail.com>
2025-03-17 22:02:32 +0530

    Update cl-kotlin-nele.kt

--- cl-kotlin-nele.kt
+++ cl-kotlin-nele.kt
@@ -1,49 +1,126 @@
-class Node<T>(var data: T) {
-    var next: Node<T>? = null
+import 'dart:io';
+
+class Car {
+  String name;
+  double price;
+
+  Car(this.name, this.price);
+
+  void changePrice(double newPrice) {
+    price = newPrice;
+    print('The price of $name has been updated to \$$price.');
+  }
 }
 
-class LinkedList<T> {
-    var head: Node<T>? = null
+class Person {
+  String name;
+  List<Car> ownedCars = [];
+  double moneyLeft;
 
-    fun insertLast(data: T) {
-        if (head == null) {
-            head = Node(data)
-        } else {
-            var current = head
-            while (current?.next != null) {
-                current = current.next
-            }
-            current?.next = Node(data)
+  Person(this.name, this.moneyLeft);
+
+  void buyCar(Car car) {
+    if (moneyLeft >= car.price) {
+      ownedCars.add(car);
+      moneyLeft -= car.price;
+      print('Congrats, $name! You just bought a ${car.name} for \${car.price}. You now have \$$moneyLeft left.');
+    } else {
+      print('Sorry, $name! You don’t have enough money to buy a ${car.name}.');
+    }
+  }
+
+  void sellCar(Car car) {
+    if (ownedCars.contains(car)) {
+      ownedCars.remove(car);
+      moneyLeft += car.price;
+      print('You sold your ${car.name} for \${car.price}. Now you have \$$moneyLeft.');
+    } else {
+      print('$name, you don’t own a ${car.name} to sell!');
+    }
+  }
+}
+
+void main() {
+  Car car1 = Car('Toyota Camry', 25000);
+  Car car2 = Car('Honda Civic', 20000);
+
+  print(' Welcome to the Car Buying and Selling Platform!\n');
+  print('What’s your name?');
+  String userName = stdin.readLineSync()!;
+  print('Hey $userName! How much money do you have?');
+  double userMoney = double.parse(stdin.readLineSync()!);
+  Person user = Person(userName, userMoney);
+
+  while (true) {
+    print('\nHere’s what you can do:');
+    print('1. Buy a car');
+    print('2. Change car price');
+    print('3. Sell a car');
+    print('4. Exit');
+    print('Enter a number to choose an option:');
+
+    int choice = int.parse(stdin.readLineSync()!);
+
+    if (choice == 1) {
+      print('\nAvailable Cars:');
+      print('1. ${car1.name} - \$${car1.price}');
+      print('2. ${car2.name} - \$${car2.price}');
+      print('Which car would you like to buy? Enter the car number:');
+      int carChoice = int.parse(stdin.readLineSync()!);
+      
+      switch (carChoice) {
+        case 1:
+          user.buyCar(car1);
+          break;
+        case 2:
+          user.buyCar(car2);
+          break;
+        default:
+          print('Oops! That’s not a valid option. Try again.');
+      }
+
+    } else if (choice == 2) {
+      print('\nWhich car’s price do you want to change?');
+      print('1. ${car1.name}');
+      print('2. ${car2.name}');
+      int carChoice = int.parse(stdin.readLineSync()!);
+      print('Enter the new price:');
+      double newPrice = double.parse(stdin.readLineSync()!);
+      
+      switch (carChoice) {
+        case 1:
+          car1.changePrice(newPrice);
+          break;
+        case 2:
+          car2.changePrice(newPrice);
+          break;
+        default:
+          print('Oops! That’s not a valid choice. Try again.');
+      }
+
+    } else if (choice == 3) {
+      if (user.ownedCars.isEmpty) {
+        print('\n$userName, you don’t have any cars to sell yet!');
+      } else {
+        print('\nHere are your cars, $userName:');
+        for (int i = 0; i < user.ownedCars.length; i++) {
+          print('${i + 1}. ${user.ownedCars[i].name}');
         }
+        print('Which one would you like to sell? Enter the car number:');
+        int sellChoice = int.parse(stdin.readLineSync()!);
+
+        if (sellChoice >= 1 && sellChoice <= user.ownedCars.length) {
+          user.sellCar(user.ownedCars[sellChoice - 1]);
+        } else {
+          print('Hmm… That’s not a valid choice. Try again.');
+        }
+      }
+
+    } else if (choice == 4) {
+      print('Thanks for visiting the Car Buying and Selling Platform, $userName! Have a great day!');
+      break;
+    } else {
+      print('That’s not a valid option. Try again.');
     }
-}
-
-fun <T> fromLast(list: LinkedList<T>, endIndex: Int): Node<T>? {
-    var fast = list.head
-    var slow = list.head
-
-    // Move fast pointer endIndex steps ahead
-    for (i in 0 until endIndex) {
-        fast = fast?.next
-    }
-
-    // Move both pointers until fast reaches the end of the list
-    while (fast?.next != null) {
-        fast = fast.next
-        slow = slow?.next
-    }
-
-    return slow
-}
-
-fun main() {
-    val l = LinkedList<Char>()
-    l.insertLast('a')
-    l.insertLast('b')
-    l.insertLast('c')
-    l.insertLast('d')
-    l.insertLast('e')
-
-    println(fromLast(l, 0)?.data) // Should print 'e'
-    println(fromLast(l, 3)?.data) // Should print 'b'
+  }
 }
